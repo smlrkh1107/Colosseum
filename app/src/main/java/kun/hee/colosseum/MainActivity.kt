8aec8ee2a08c07b74dcec8fe3e93f2422cf1e996
smlrkh1107 <krae7789230@gmail.com>
2020-05-12 21:35:51 +0900

    로그인 실패시 토스트 처리

--- app/src/main/java/kun/hee/colosseum/MainActivity.kt
+++ app/src/main/java/kun/hee/colosseum/MainActivity.kt
@@ -2,6 +2,12 @@ package kun.hee.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kun.hee.colosseum.utils.ServerUtil
+import org.json.JSONObject
+import kotlin.math.log
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +18,30 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            val email = emailEdt.text.toString()
+            val pw = pwEdt.text.toString()
+
+            ServerUtil.postRequestLogin(mContext, email, pw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+//                    Log.d("로그인 응답", json.toString())
+                    val code = json.getInt("code")
+
+                    if(code == 200){
+
+                    }
+                    else {
+                        val message = json.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+            })
+
+        }
 
     }
 
